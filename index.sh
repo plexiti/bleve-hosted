5da339cc321b3dc85c7dc82c79e7293c45f5b80b
Martin Schimak <martin.schimak@plexiti.com>
2016-12-17 19:15:52 +0100

    Updated indexes to reflect branch urls

--- index.sh
+++ index.sh
@@ -14,9 +14,15 @@ for b in `git branch --remote | awk -F ' +' '! /\(no branch\)/ {print $2}'`; do
   if [ ${b##*/} != "HEAD" ]
   then
       git checkout -B ${b##*/} $b
+      if [ ${b##*/} != "master" ]
+      then
+        sed -i -- "s/camunda\.com\/best-practices/camunda\.com\/best-practices-branch\/${b##*\/}/g" config.yaml
+      fi
       hugoidx
       mv search.bleve "../indexes/${b##*/}"
       rm -rf search.bleve
+      git checkout .
+      git clean -f
   fi
 done
 
