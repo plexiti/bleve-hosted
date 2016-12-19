5ee518618281600654365383af07b2253fd0f60f
Martin Schimak <martin.schimak@plexiti.com>
2016-12-19 15:09:10 +0100

    feature(search): remove each index separately

--- index.sh
+++ index.sh
@@ -6,7 +6,6 @@ then
     git clone git@github.com:camunda/best-practices.git best-practices
 fi
 mkdir -p indexes
-rm -rf indexes/*
 cd best-practices
 git fetch --all --prune
 
@@ -20,6 +19,7 @@ for b in `git branch --remote | awk -F ' +' '! /\(no branch\)/ {print $2}'`; do
         sed -i -- "s/camunda\.com\/best-practices/camunda\.com\/best-practices-branch\/${BRANCH}/g" config.yaml
       fi
       grep "^\s*draft:\s*true\s*$" ./best-practices/content --include="index.adoc" -Rl | xargs rm $1
+      rm -rf ../indexes/$BRANCH
       hugoidx
       mv search.bleve "../indexes/${BRANCH}"
       rm -rf search.bleve
